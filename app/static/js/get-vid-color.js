398e986a22a1e750403dc68483a77eafb83f3c5e
dannypernik <dannypernik@mail.com>
2023-01-19 21:21:40 -0600

    dynxamic color on homepage

--- app/static/js/get-vid-color.js
+++ app/static/js/get-vid-color.js
@@ -1,29 +1,28 @@
 // Adapted from https://reustle.org/reading-html5-video-data
 
 // Main elements
-var body = document.getElementsByTagName('body')[0];
-var current_rgb = document.getElementById('current_rgb');
-var my_video = document.getElementById('bg_vid');
-var my_canvas = document.getElementById('my_canvas');
+// var body = document.getElementsByTagName('body')[0];
+// var current_rgb = document.getElementById('current_rgb');
+var my_video = document.getElementById('bg-vid');
+var my_canvas = document.getElementById('my-canvas');
 var my_canvas_context = my_canvas.getContext('2d');
 
 var update_bg = function(){
-  
   // If the video isn't playing, don't loop
-  if(my_video.paused || my_video.ended){
-    return false;
-  }
-  
+  // if(my_video.paused || my_video.ended){
+  //   return false;
+  // }
+
   // Draw the current frame of the video onto the hidden canvas
-  my_canvas_context.drawImage(my_video, 0, 0, my_video.width/2, my_video.height/2);
-  
+  my_canvas_context.drawImage(my_video, 0, 0, window.innerWidth/2, window.innerHeight/2);
+
   // Pull the image data from the canvas
-  var frame_data = my_canvas_context.getImageData(0, 0, my_video.width/2, my_video.height/2).data;
-  
+  var frame_data = my_canvas_context.getImageData(0, 0, window.innerWidth/2, window.innerHeight/2).data;
+
   // Get the length of the data, divide that by 4 to get the number of pixels
   // then divide that by 4 again so we check the color of every 4th pixel
   var frame_data_length = (frame_data.length / 4) / 4;
-  
+
   // Loop through the raw image data, adding the rgb of every 4th pixel to rgb_sums
   var pixel_count = 0;
   var rgb_sums = [0, 0, 0];
@@ -33,30 +32,29 @@ var update_bg = function(){
     rgb_sums[2] += frame_data[i*4+2];
     pixel_count++;
   }
-  
+
   // Average the rgb sums to get the average color of the frame in rgb
-  rgb_sums[0] = Math.floor(rgb_sums[0]/pixel_count);
-  rgb_sums[1] = Math.floor(rgb_sums[1]/pixel_count);
-  rgb_sums[2] = Math.floor(rgb_sums[2]/pixel_count);
-  
+  rgb_sums[0] = Math.floor(255  - rgb_sums[0]/pixel_count);
+  rgb_sums[1] = Math.floor(255 - rgb_sums[1]/pixel_count);
+  rgb_sums[2] = Math.floor(255 - rgb_sums[2]/pixel_count);
+
   // Set the background color to the new color
   var new_rgb = 'rgb(' + rgb_sums.join(',') + ')';
-  body.style.background = new_rgb;
-  
-  // Update the rgb label
-  current_rgb.innerHTML = new_rgb;
-  
+  document.documentElement.style.setProperty('--dynamic-color', new_rgb );
+
+  // // Update the rgb label
+  // current_rgb.innerHTML = new_rgb;
+
   // Repeat every 1/10th of a second
   setTimeout(update_bg, 100);
 }
 
 var init = function(){
   // Update the size of the canvas to match the video
-  my_canvas.width = my_video.width/2;
-  my_canvas.height = my_video.height/2;
-  
+  my_canvas.width = window.innerWidth/2;
+  my_canvas.height = window.innerHeight/2;
   // Start updating the bg color
   update_bg();
 }
 
-my_video.addEventListener('play', init);
+document.addEventListener("DOMContentLoaded", init);
